6f46ce4c158ce03c52dfb4161c053854ac260abd
jiyaushah <jiyautpalshah@gmail.com>
2024-10-15 23:20:39 -0400

    signup-->auth, login functionality
    login modal works, changed signup.js to auth (so it can be more general).
    
    bugs: need to close the nav bar when opening a modal

--- JS/auth.js
+++ JS/auth.js
@@ -2,34 +2,11 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/fireba
 import { getFirestore, doc, getDoc, setDoc, collection } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js"
 import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from   "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js"; 
 
-
-const signupButton = document.getElementById("signup-button");
-const closeSignupButton = document.getElementById("close-signup-button");
-const signupModal = document.getElementById("signup-modal");
-const navsignup = document.getElementById("getstarted-signup");
-
-
-console.log(signupModal)
-
-
-function openSignupModal() {
-    signupModal.style.display = 'block';
-}
-function closeSignupModal() {
-    signupModal.style.display = 'none';
-}
-
-signupButton.addEventListener('click', openSignupModal);
-closeSignupButton.addEventListener('click', closeSignupModal);
-navsignup.addEventListener('click', openSignupModal);
-
-
 //error pop-ups
 const popupOverlay = document.getElementById('popupOverlay');
 const popup = document.getElementById('popup');
 const closePopup = document.getElementById('closePopup');
 // Function to open the popup
-
 function openPopup() {
     popup.style.display = 'block';
 }
@@ -93,6 +70,21 @@ auth.onAuthStateChanged(user => {
         });
     }
 });
+
+//signup functionalities
+const signupButton = document.getElementById("signup-button");
+const closeSignupButton = document.getElementById("close-signup-button");
+const signupModal = document.getElementById("signup-modal");
+const navsignup = document.getElementById("getstarted-signup");
+function openSignupModal() {
+    signupModal.style.display = 'block';
+}
+function closeSignupModal() {
+    signupModal.style.display = 'none';
+}
+signupButton.addEventListener('click', openSignupModal);
+closeSignupButton.addEventListener('click', closeSignupModal);
+navsignup.addEventListener('click', openSignupModal);
 
 //sign-up
 const signupform = document.querySelector('#signup-form');
@@ -160,6 +152,60 @@ signupform.addEventListener('submit', (e) =>{
         }    
     });
 } ); //when the form submits, we want to add their user if it doesn't exist
+
+// login functionalities
+const loginButton = document.getElementById("login-button");
+const closeLoginButton = document.getElementById("close-login-button");
+const loginModal = document.getElementById("login-modal");
+function openLoginModal() {
+    loginModal.style.display = 'block';
+}
+function closeLoginModal() {
+    loginModal.style.display = 'none';
+}
+loginButton.addEventListener('click', openLoginModal);
+closeLoginButton.addEventListener('click', closeLoginModal);
+
+//login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+e.preventDefault();
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  signInWithEmailAndPassword(auth, email, password).then((cred) => {
+        // close the signup modal & reset form
+        const modal = document.querySelector('#modal-login');
+        loginForm.reset();
+        closeLoginModal();
+      }).catch ((e) =>{
+        if (e.code === 'auth/invalid-email') {
+            const element = document.getElementById("signupErrorContent");
+            element.textContent = "Invalid Email."; 
+            openPopup();
+        } else if (e.code === 'auth/user-not-found') {
+            const element = document.getElementById("signupErrorContent");
+            element.textContent = "User not found."; 
+            openPopup();
+        } else if (e.code === 'auth/invalid-credential') {
+            const element = document.getElementById("signupErrorContent");
+            element.textContent = "Invalid Credentials. Maybe an incorrect email or password?"; 
+            openPopup();
+        } else if(e.code === 'auth/too-many-requests'){
+            const element = document.getElementById("signupErrorContent");
+            element.textContent = "Too many failed login attempts. Reset password or try again later."; 
+            openPopup();
+        }
+        else {
+            alert(e.message);
+        }   
+    });
+  
+
+});
+
 
 //logout
 const logout = document.querySelector('#logout');
